dcb8c347231a8f7427231dea65e418b3c8df37a3
s.m.israt <smisratara25@gmail.com>
2023-06-19 19:52:58 +0600

    solved error: Assertion problem flutter finally!!!1

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+// TODO Implement this library.
+ class Constants {
+  static String appId = " ";
+  static String apiKey = " ";
+  static String messagingSenderId = " ";
+  static String projectId = " ";
+}
